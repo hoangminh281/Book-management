9c15914c077bebf070652f43bd13bb122bb7a555
hoangminh281 <tranhoangminhptit@gmail.com>
2018-05-08 21:26:09 +0700

    Update connect server

--- Quanlibansach/Program.cs
+++ Quanlibansach/Program.cs
@@ -19,33 +19,33 @@ namespace Quanlibansach
 
         public static String image_address = "https://sledge-hammer-liquo.000webhostapp.com/public/img/";
 
-        private static String server_address = "http://thoitranggymer.com";
+        private static String server_address = "http://cuongmanh2311.000webhostapp.com/";
         
 
-        public static String path_AllCategories = server_address + "/categories";
-        public static String path_AllProducts = server_address + "/products/getall";
-        public static String path_productsFromCategory = server_address + "/products/cate_product?cate_id=";
-        public static String path_storeProduct = server_address + "/products/store";
-        public static String path_updateProduct = server_address + "/products/update";
-        public static String path_destroyProduct = server_address + "/products/destroy/";
-        public static String path_signin = server_address + "/users/login";
-        public static String path_AllPermission = server_address + "/permission";
-        public static String path_getPermissionbelow = server_address + "/users/permission";
-        public static String path_getPermission = server_address + "/permission/show/";
-        public static String path_storeUser = server_address + "/users/store";
-        public static String path_updateUser = server_address + "/users/update";
-        public static String path_destroyUser = server_address + "/users/destroy/";
-        public static String path_getUserbelow = server_address + "/users/user_permission";
-        public static String path_storeCategory = server_address + "/categories/store";
-        public static String path_updateCategory = server_address + "/categories/update";
-        public static String path_destroyCategory = server_address + "/categories/destroy/";
-        public static String path_AllRent = server_address + "/rent";
-        public static String path_storeRent = server_address + "/rent/store";
-        public static String path_updateRent = server_address + "/rent/update";
-        public static String path_destroyRent = server_address + "/rent/destroy/";
-        public static String path_storePermission = server_address + "/permission/store";
-        public static String path_updatePermission = server_address + "/permission/update";
-        public static String path_destroyPermission = server_address + "/permission/destroy/";
+        public static String path_AllCategories = server_address + "categories";
+        public static String path_AllProducts = server_address + "products/getall";
+        public static String path_productsFromCategory = server_address + "products/cate_product?cate_id=";
+        public static String path_storeProduct = server_address + "products/store";
+        public static String path_updateProduct = server_address + "products/update";
+        public static String path_destroyProduct = server_address + "products/destroy/";
+        public static String path_signin = server_address + "users/login";
+        public static String path_AllPermission = server_address + "permission";
+        public static String path_getPermissionbelow = server_address + "users/permission";
+        public static String path_getPermission = server_address + "permission/show/";
+        public static String path_storeUser = server_address + "users/store";
+        public static String path_updateUser = server_address + "users/update";
+        public static String path_destroyUser = server_address + "users/destroy/";
+        public static String path_getUserbelow = server_address + "users/user_permission";
+        public static String path_storeCategory = server_address + "categories/store";
+        public static String path_updateCategory = server_address + "categories/update";
+        public static String path_destroyCategory = server_address + "categories/destroy/";
+        public static String path_AllRent = server_address + "rent";
+        public static String path_storeRent = server_address + "rent/store";
+        public static String path_updateRent = server_address + "rent/update";
+        public static String path_destroyRent = server_address + "rent/destroy/";
+        public static String path_storePermission = server_address + "permission/store";
+        public static String path_updatePermission = server_address + "permission/update";
+        public static String path_destroyPermission = server_address + "permission/destroy/";
 
         public static Category[] getAllCategory()
         {
